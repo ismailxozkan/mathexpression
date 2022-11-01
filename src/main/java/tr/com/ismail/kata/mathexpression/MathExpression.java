2365a4f44ec3724dd8b6ee356cc054440144203e
ismail ozkan <ismailxozkan@gmail.com>
2022-11-01 16:47:29 +0300

    First commit

---
+++ src/main/java/tr/com/ismail/kata/mathexpression/MathExpression.java
@@ -0,0 +1,19 @@
+package tr.com.ismail.kata.mathexpression;
+
+// a Matehematical expression in infix order is  easily readible by human.
+// For computing a represantation of postfix order is more efficient.
+// To evaluate infix expressions requires multiple time searching from start to end expression
+// so first step would be respresenting the expression in postfix.
+// than we will evaluate postfix expression using stack or binary expression tree
+// then we will compare the results using complexity measures
+
+
+public class MathExpression {
+
+    public double calculate(String expression) {
+        return 0.0d;
+    }
+
+
+
+}
